20ff1a466f83b28f9b37877959e0fc5f296e27dc
kongfanteng <18210591009@163.com>
2023-08-13 10:58:57 +0800

    feat: Promise 学习-准备

---
+++ 1.prepare.js
@@ -0,0 +1,27 @@
+/**
+ * @example
+ *
+ * ```js
+    // Promise 学习-准备
+    // - 1、需要安装 vscode
+    // - - live server 启动浏览器，通过 localhost 查看
+    // - - code running 运行部分 js 代码
+
+    // - 2、git 码云
+    // - - Mac 安装 git
+    brew install git
+    // - - Window 安装 git：下载安装包
+
+    // - 3、Node
+    {@link https://nodejs.org/en}
+
+    // - 4、学习目录
+    // - - callback
+    // - - Promise 应用
+    // - - 手写 Promise
+    // - - Promise 方法：all-race-finally
+    // - - generator + co => async + await
+
+ * ```
+ * 
+ */
